90d7822327d283f7cb48d578536aeff7fc3edd11
Your Name <wrnermontala@gmail.com>
2022-12-12 20:31:35 +0800

    add login/logout function for trader

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  enum :user_type, { trader: 0, admin: 1 }
 end
