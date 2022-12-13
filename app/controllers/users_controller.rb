7a581f82015737fe40d47df43dff534ac75236c8
Dan <vareladanmarj@gmail.com>
2022-12-13 22:54:47 +0800

    CRUD users as an admin

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,61 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :get_user, only: %i[show edit update destroy]
+  before_action :get_user_type_options, only: %i[new create edit update]
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+
+    if @user.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show; end
+
+  def edit; end
+
+  def update
+    if @user.update optional_password_params
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user.destroy!
+    redirect_to root_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :user_type)
+  end
+
+  def optional_password_params
+    custom_params = user_params
+    custom_params[:password] = nil if user_params[:password].blank?
+    custom_params[:password_confirmation] = nil if user_params[:password_confirmation].blank?
+    custom_params
+  end
+
+  def get_user
+    @user = User.find params[:id]
+  end
+
+  def get_user_type_options
+    @user_type_options = User.user_types.keys.map { |type| [type.humanize, type] }
+  end
+end
