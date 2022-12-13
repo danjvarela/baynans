dc226d2a1d76fc9e3d75741961d3d652c41bd51e
Dan <vareladanmarj@gmail.com>
2022-12-13 20:23:27 +0800

    add user type test

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  let(:user) { create :user }
+
+  describe 'User type' do
+    it 'should have a default value of `trader`' do
+      created_user = user
+      expect(created_user.trader?).to be true
+    end
+  end
+end
