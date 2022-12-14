865460c1ae1c10b280dc461cf677b0314010441f
Your Name <wrnermontala@gmail.com>
2022-12-14 23:10:59 +0800

    add trader specs

--- spec/requests/traders_spec.rb
+++ spec/requests/traders_spec.rb
@@ -1,9 +1,34 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe "Traders", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  let(:user) { create :user }
+  let(:user_attributes) { attributes_for :user }
+
+  describe "GET /admin/users" do
+    it "should redirect to root path" do
+      get users_path
+      expect(response).to redirect_to root_path
+    end
   end
 
-  
+  describe "POST /admin/users" do
+    it "should redirect to root path" do
+      post users_path, params: {user: user_attributes}
+      expect(response).to redirect_to root_path
+    end
+  end
+
+  describe "DELETE /admin/users" do
+    it "should redirect to root path" do
+      delete users_path
+      expect(response).to redirect_to root_path
+    end
+  end
+
+  describe "PUT /admin/users" do
+    it "should redirect to root path" do
+      put users_path, params: {user: user_attributes}
+      expect(response).to redirect_to root_path
+    end
+  end
 end
