7179ac81344e85c4905ecad283ac16e00631551c
Dan <vareladanmarj@gmail.com>
2022-12-17 10:46:19 +0800

    update user model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,12 +13,12 @@ RSpec.describe User, type: :model do
   describe "signup status" do
     it "should have a pending value by default if user is a trader" do
       created_user = user
-      expect(created_user.pending?).to be true
+      expect(created_user.trading_status_pending?).to be true
     end
 
     it "should be approved by default if user is an admin" do
       created_user = create :user, {user_type: :admin}
-      expect(created_user.approved?).to be true
+      expect(created_user.trading_status_approved?).to be true
     end
   end
 end
