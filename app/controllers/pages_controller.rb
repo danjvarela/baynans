3cfe764e42b36d5e35583cc52771fdff43b4e532
Dan <vareladanmarj@gmail.com>
2023-01-05 18:19:06 +0800

    feat: show list of active stocks at the home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
   def home
+    @stocks = Stock.all
+
+    client = Iex.client
+    # list of active stocks
+    @stock_market_list = client.stock_market_list(:mostactive)
   end
 end
