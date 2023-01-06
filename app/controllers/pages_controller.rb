96902fc1f23cdb3f492c88d7ee529b553e5884db
Dan <vareladanmarj@gmail.com>
2023-01-06 08:17:17 +0800

    fix: use the list of most active stocks instead of the stocks from database
    Instead of getting the stocks from the database, use the IEX client's
    list of active stocks.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,5 @@
 class PagesController < ApplicationController
   def home
-    @stocks = Stock.all
-
     client = Iex.client
     # list of active stocks
     @stock_market_list = client.stock_market_list(:mostactive)
