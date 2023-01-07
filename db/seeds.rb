ff048bbd7619b53f6dd67242c649d47259f34069
Dan <vareladanmarj@gmail.com>
2023-01-07 08:31:10 +0800

    chore: update seeds to include transactions and make it runnable multiple times

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,22 +8,22 @@
 # User.create email: "trader1@example", password: "password"
 # User.create email: "admin1@example", password: "password", user_type: :admin
 
-admin = User.new(email: "admin@example.com", password: "qwerty", user_type: :admin)
+admin = User.new email: "admin@example.com", password: "qwerty", user_type: :admin
 admin.skip_confirmation!
-admin.save!
+admin.save! if !User.find_by email: admin.email
 
-client = Iex.client
-stock_market_list = client.stock_market_list(:mostactive)
-
-stock_market_list.each do |stock|
-  Stock.create(symbol: stock["symbol"], company_name: stock["company_name"])
+Iex.most_active_stocks.each do |stock|
+  Stock.create symbol: stock["symbol"], company_name: stock["company_name"] unless Stock.find_by symbol: stock["symbol"]
 end
 
 stocks = Stock.all
 
 10.times do |n|
-  user = User.new(email: "user#{n}@example.com", password: "qwerty")
+  user = User.new email: "user#{n}@example.com", password: "qwerty"
   user.skip_confirmation!
-  user.save!
-  user.stocks << stocks.sample(3)
+  user.save if !User.find_by email: user.email
+
+  3.times do
+    Transaction.create user: user, stock: stocks.sample, amount: rand(1.5..3.0), transaction_type: [:buy, :sell].sample
+  end
 end
