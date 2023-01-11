01413c8ed96cfa7befbd6c9dead1c13a6a048390
Dan <vareladanmarj@gmail.com>
2023-01-11 18:48:06 +0800

    fix: remove iex client logger. use it only for tests.

--- app/models/iex.rb
+++ app/models/iex.rb
@@ -1,10 +1,8 @@
 class Iex < ApplicationRecord
   def self.client
-    logger_instance = Logger.new(STDOUT)
     IEX::Api::Client.new(
       publishable_token: ENV['iex_publishable_token'],
       endpoint: 'https://cloud.iexapis.com/v1',
-      logger: logger_instance
     )
   end
 
